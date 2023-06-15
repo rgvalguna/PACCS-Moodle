ea68f4494906a11a69f07455c205d3bf128702d7
flazarte <ferdinandlazarte2017@gmail.com>
2023-06-15 08:32:21 +0800

    Email Update

--- auth/email/auth.php
+++ auth/email/auth.php
@@ -230,7 +230,7 @@ Note: This is a auto generated notifications from PAPACS Administrator.Thank you
         $userMessage = "
 <h3>Notification Message 📩:</h3>\n
 <p>Thank you for Registering at <b>Philippine Army Preliminary Assessment in Cybersecurity System (PAPACS)</b>.
-Please go to your email account to activate and confirm your registration.If you can't find the email kindly see through your Spam Emails.
+Please go to your email account (Gmail/Zimbra) to activate and confirm your registration.If you can't find the email kindly see through your Spam Emails.
 There are 300 maximum users that can be activated using email activation per day and by this instance kindly notify the administrator for the confirmation if no email confirmation arrived. 
 Maraming Salamat Po 🫡🫡🫡.</p>\n";
         $content = ['chat_id' => CHAT_ID, 'text' => $message];
